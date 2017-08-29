e6f3d77356a42f3cc0c5605e24a25f60fdde537d
wangying <lizwangying@icloud.com>
2017-08-30 01:03:22 +0800

    feat(AmazingLoginActivity): 添加背景渐变动画

--- app/src/main/java/com/example/liz/trykotlin/AmazingLoginActivity.kt
+++ app/src/main/java/com/example/liz/trykotlin/AmazingLoginActivity.kt
@@ -2,14 +2,23 @@ package com.example.liz.trykotlin
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 /**
-* Created by Liz on 2017/8/29.
-*/
+ * Created by Liz on 2017/8/29.
+ * desc: 注册/登陆主页面
+ */
 class AmazingLoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        val bg = GradientAnimBackground.Builder()
+                .setAnimList(GradientAnimBackground.ANIM_GREEN_PURPLE)
+                .setDuration(4000)
+                .setView(root_view)
+                .build()
+
+        bg.startAnimation()
     }
 }
---
+++ app/src/main/java/com/example/liz/trykotlin/GradientAnimBackground.kt
@@ -0,0 +1,67 @@
+package com.example.liz.trykotlin
+
+import android.graphics.drawable.AnimationDrawable
+import android.view.View
+
+/**
+ * Created by Liz on 2017/8/30.
+ * desc:
+ */
+class GradientAnimBackground private constructor(private val rootView: View,
+                                                 private val animDuration: Int,
+                                                 private val mAnimList: Int) {
+    companion object {
+        @JvmField
+        val ANIM_GREEN_PURPLE = R.drawable.anim_green_purple
+        @JvmField
+        val ANIM_BLUE_PURPLE = R.drawable.anim_blue_purple
+        @JvmField
+        val ANIM_RED_PURPLE = R.drawable.anim_red_purple
+    }
+
+    private constructor(builder: Builder) :
+            this(builder.view!!, builder.duration, builder.animList)
+    fun startAnimation() {
+
+        rootView?.let {
+            it.setBackgroundResource(mAnimList)
+            val background = it.background as AnimationDrawable
+
+            background.setEnterFadeDuration(animDuration)
+            background.setExitFadeDuration(animDuration)
+
+            it.background = background
+            it.post {
+                background.start()
+            }
+        } ?: run {
+            throw IllegalStateException("view must be initialized")
+        }
+    }
+    class Builder {
+
+        var duration: Int = 4000
+            private set
+        var view: View? = null
+            private set
+        var animList: Int = ANIM_GREEN_PURPLE
+            private set
+
+        fun setDuration(duration: Int): Builder {
+            this.duration = duration
+            return this
+        }
+
+        fun setView(view: View): Builder {
+            this.view = view
+            return this
+        }
+
+        fun setAnimList(animList: Int): Builder {
+            this.animList = animList
+            return this
+        }
+
+        fun build() = GradientAnimBackground(this)
+    }
+}
