be723834223fc8b98ae753b98a52b7a64da040e6
wangying <lizwangying@icloud.com>
2017-08-30 00:29:21 +0800

    feat(AmazingLoginActivity):修改主布局

---
+++ app/src/main/java/com/example/liz/trykotlin/AmazingLoginActivity.kt
@@ -0,0 +1,15 @@
+package com.example.liz.trykotlin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+* Created by Liz on 2017/8/29.
+*/
+class AmazingLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+    }
+}
