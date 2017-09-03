4ee7196496d0b0d2727af2d2704d44d6b51820c1
wangying <lizwangying@icloud.com>
2017-09-04 01:51:53 +0800

    feat(AmazingLoginActivity):模拟注册成功

--- app/src/main/java/com/example/liz/trykotlin/AmazingLoginActivity.kt
+++ app/src/main/java/com/example/liz/trykotlin/AmazingLoginActivity.kt
@@ -1,22 +1,36 @@
 package com.example.liz.trykotlin
 
+import android.content.Context
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextUtils
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import kotlinx.android.synthetic.main.activity_login.*
+import java.util.regex.Pattern
 
 /**
  * Created by Liz on 2017/8/29.
  * desc: 注册/登陆主页面
  */
-class AmazingLoginActivity : AppCompatActivity() {
+class AmazingLoginActivity : AppCompatActivity(), View.OnClickListener {
+    private var userInfo = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         setAmazingBg()
+        setListener()
+
+
     }
 
-    fun setAmazingBg() {
+    private fun setAmazingBg() {
         val bg = GradientAnimBackground.Builder()
                 .setAnimList(GradientAnimBackground.ANIM_GREEN_PURPLE)
                 .setDuration(4000)
@@ -24,5 +38,89 @@ class AmazingLoginActivity : AppCompatActivity() {
                 .build()
 
         bg.startAnimation()
+    }
+
+    private fun setListener() {
+        im_clear_email.setOnClickListener(this)
+        im_clear_pwd.setOnClickListener(this)
+        email_sign_in_button.setOnClickListener(this)
+
+        email.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {
+            }
+
+            override fun onTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {
+            }
+
+            override fun afterTextChanged(editable: Editable) {
+                var pattern: Pattern = Pattern.compile("^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\\.[a-zA-Z0-9_-]+)+\$")
+                if (!TextUtils.isEmpty(email.text)) {
+                    var str = email.text
+                    if (!pattern.matcher(str).matches()) {
+                        email.error = "邮箱格式不正确"
+                    } else {
+                        email.error = null
+                    }
+                }
+                controlClear(!TextUtils.isEmpty(email.text), im_clear_email, email)
+
+            }
+        })
+        email.setOnFocusChangeListener({ _, hasFocus ->
+            controlClear(hasFocus && !TextUtils.isEmpty(email.text), im_clear_email, email)
+        })
+        password.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                var pattern: Pattern = Pattern.compile("^.{6,}\$")
+                if (!TextUtils.isEmpty(password.text)) {
+                    var str = password.text
+                    if (!pattern.matcher(str).matches()) {
+                        password.error = "密码长度最少6位"
+                    } else {
+                        password.error = null
+                    }
+                }
+            }
+        })
+        password.setOnFocusChangeListener({ _, hasFocus ->
+            controlClear(hasFocus && !TextUtils.isEmpty(password.text), im_clear_pwd, password)
+        })
+    }
+
+
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.im_clear_email -> email.setText("")
+            R.id.im_clear_pwd -> password.setText("")
+            R.id.email_sign_in_button -> {
+                if (TextUtils.isEmpty(email.text)) {
+                    email.error = "请输入邮箱哦~"
+                }
+                if (TextUtils.isEmpty(password.text)) {
+                    password.error = "请输入密码哦~"
+                }
+                var manager: InputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                manager.hideSoftInputFromWindow(window.decorView.windowToken, 0)
+
+
+                Snackbar.make(root_view, "注册成功", Snackbar.LENGTH_SHORT).show()
+            }
+
+        }
+    }
+
+    private fun controlClear(clear: Boolean, clearView: View, editText: EditText) {
+        if (clear) {
+            clearView.visibility = View.VISIBLE
+        } else {
+            clearView.visibility = View.GONE
+            editText.error = null
+        }
     }
 }
--- app/src/main/java/com/example/liz/trykotlin/EmailAutoCompleteTv.kt
+++ app/src/main/java/com/example/liz/trykotlin/EmailAutoCompleteTv.kt
@@ -24,35 +24,19 @@ class EmailAutoCompleteTv(context: Context?, attrs: AttributeSet?) : AutoComplet
     init {
         this.setAdapter(EmailAutoCompleteAdapter(context, R.layout.item_layout_autocomplete_tv, emailSufixs))
         this.threshold = 1
-        this.setOnFocusChangeListener { view, hasFocus ->
-            if (hasFocus) {
-                var text: String = this@EmailAutoCompleteTv.text.toString()
-                if (android.text.TextUtils.isEmpty(text))
-                    performFiltering(text, 0)//当该文本重新获得焦点后，重启自动完成
-            } else {
-                //当文本输入完成后，检查输入email地址的格式
-                var ev: EmailAutoCompleteTv = view as EmailAutoCompleteTv
-                var text: String = ev.text.toString()
-                var pattern: Pattern = Pattern.compile("\"^([a-zA-Z0-9_\\-\\.]+)@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([a-zA-Z0-9\\-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\\]?)\$\"")
-                if (!(!android.text.TextUtils.isEmpty(text) && pattern.matcher(text).matches())) {
-                    this@EmailAutoCompleteTv.error = "邮箱格式不正确"
-                }
-            }
-
-        }
     }
 
 
     override fun replaceText(text: CharSequence?) {
-        var t: String = this.text.toString()
+        var t: String = getText().toString()
         var index: Int = t.indexOf("@")
-        if (index != -1) t = text!!.substring(0, index)
+        if (index != -1) t = t!!.substring(0, index)
         super.replaceText(t + text)
     }
 
     override fun performFiltering(text: CharSequence?, keyCode: Int) {
         //在用户输入完成后调用，将已经出入的文本和adapter中的数据对比，若匹配到adapter中数据的前半部分，则adapter中的这条数据就会在下拉框中出现
-        var t = text.toString()
+        var t = getText().toString()
         var p: Pattern = Pattern.compile("^[a-zA-Z0-9_]+$")
         if (t.indexOf("@") == -1) {
             if (p.matcher(t).matches()) {
@@ -68,32 +52,6 @@ class EmailAutoCompleteTv(context: Context?, attrs: AttributeSet?) : AutoComplet
     }
 
     private inner class EmailAutoCompleteAdapter(context: Context?, resource: Int, objects: Array<out String>?) : ArrayAdapter<String>(context, resource, objects) {
-
-//        var holder: ViewHolder? = null
-//        @SuppressLint("SetTextI18n")
-//        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-//            var view: View? = convertView
-//            if (view == null) {
-//                view = LayoutInflater.from(context).inflate(R.layout.item_layout_autocomplete_tv, null)
-//                view!!.tag = holder
-//            } else {
-//                holder = view.tag as ViewHolder
-//            }
-//            holder!!.tv = item_email_tv
-//            var text: String = this@EmailAutoCompleteTv.text.toString()
-//            var index: Int = text.indexOf("@")
-//            if (index != -1) {
-//                text = text.substring(0, index)
-//            }
-//            //将用户输入的字符串和邮箱后缀拼接，在下拉框中显示
-//            holder!!.tv.text = """$text${getItem(position)}"""
-//            return view
-//
-//        }
-//
-//        inner class ViewHolder {
-//            lateinit var tv: TextView
-//        }
 
         @SuppressLint("SetTextI18n")
         override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
